19700096bc9ad2f2e02e2d9483f71b2260a7ecd8
eltacodeldiablo <smiley.we@gmail.com>
2014-01-24 23:35:45 -0500

    raycast cone

--- Assets/Scripts/Cone.cs
+++ Assets/Scripts/Cone.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Cone : MonoBehaviour {
-	float playerFOV = 1.0f;//40.0f;
+	float playerFOV = 40.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,7 +11,11 @@ public class Cone : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// Debug.DrawLine(new Vector3(0, 0, 0), new Vector3(10,10,10), Color.red,10);
+
+		//raycast view cone
+		//green if not hitting
+		this.transform.renderer.material.color = Color.green;
+
 		for(int i = 0; i < playerFOV; i++) {//Raycast every 1 degree in FOV
 			float theta = Mathf.Deg2Rad*(-1*playerFOV/2+i);//Direction of vector
             
@@ -22,21 +26,12 @@ public class Cone : MonoBehaviour {
             Vector2 rayDir = new Vector2(fwdVec.x * cs - fwdVec.y * sn, fwdVec.x * sn + fwdVec.y * cs);
 			RaycastHit2D hitObj = Physics2D.Raycast(this.transform.position, rayDir);
 			if(hitObj.transform != null ){
-				Debug.Log("hit "+hitObj.transform.name);//"Obj hit @ " + Mathf.Rad2Deg*(theta) + "Degrees!");
+				//red if hitting an object
+				this.transform.renderer.material.color = Color.red;
 			}
-			
+
 			Debug.DrawLine(this.transform.position, new Vector3(10*rayDir.x, 10*rayDir.y, 0), Color.red);
 		}
 	}
    
-
-	// void OnTriggerEnter2D(Collider2D col) {
-	// 	//Trigger Enter
-	// 	Transform objTransform = col.gameObject.transform;
-	// 	Vector3 playerPos = this.transform.position;
-	// 	float objAngleAway = Vector3.Angle(this.transform.forward, playerPos - objTransform.position);
-	// 	if(objAngleAway > playerFOV/2) Debug.Log("Colliding, not in cone");
-	// 	else Debug.Log("IN CONE!!!");
-	// }
-
 }
