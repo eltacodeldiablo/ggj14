de9a4705b1770c18be513550ee3e9a52de85b7a4
neonwednesdays <dxiao7@gatech.edu>
2014-01-25 18:09:35 -0500

    Flashlight and light deformation now works! Only "Block" tag gets changed now.

--- Assets/Scripts/Cone.cs
+++ Assets/Scripts/Cone.cs
@@ -2,22 +2,20 @@ using UnityEngine;
 using System.Collections;
 
 public class Cone : MonoBehaviour {
-	public float playerFOV = 40.0f;
-	public float viewDist = 4f;
-	public float degBtwnRays = 1f;
+        public float playerFOV = 40.0f;
+        public float viewDist = 4f;
+        public float degBtwnRays = 1f;
     public GameObject player;
 
-	private Vector3[] newVertices;
+        private Vector3[] newVertices;
     private Vector2[] newUV;
     private int[] newTriangles;
 
     void Start() {
         Mesh mesh = new Mesh();
-        this.gameObject.AddComponent("MeshFilter");
-        this.gameObject.AddComponent("MeshRenderer");
-        GetComponent<MeshFilter>().mesh = mesh;
         
-		newVertices = new Vector3[Mathf.FloorToInt((playerFOV/degBtwnRays)+1)];
+        
+        newVertices = new Vector3[Mathf.FloorToInt((playerFOV/degBtwnRays)+1)];
         newUV = new Vector2[newVertices.Length];
         newTriangles = new int[(newVertices.Length-1)*3];
 
@@ -27,75 +25,78 @@ public class Cone : MonoBehaviour {
         mesh.uv = newUV;
         mesh.triangles = newTriangles;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    //RAYCAST VIEW CONE
-		//green if not hitting
-		player.transform.renderer.material.color = Color.green;
-		
-		//MESH INIT
-		Mesh mesh = GetComponent<MeshFilter>().mesh;
-		mesh.Clear();
-        newVertices[0] = this.transform.position;
-        // Debug.Log(newVertices.Length);
-        newUV[0] = new Vector2(newVertices[0].x, newVertices[0].y);
         
-		for(float i = 0f; i < playerFOV; i+=degBtwnRays) {//Raycast every 1 degree in FOV
-			//RAYCAST INIT
-			int intI = Mathf.FloorToInt(i);
-			float theta = Mathf.Deg2Rad*(-1*playerFOV/2+i);//Direction of vector
-			float cs = Mathf.Cos(theta);
-			float sn = Mathf.Sin(theta);
+    // Update is called once per frame
+    void Update () {
+        //RAYCAST VIEW CONE
+            //green if not hitting
+            player.transform.renderer.material.color = Color.green;
             
-			Vector3 fwdVec = this.transform.up;
-            Vector2 rayDir = new Vector2(fwdVec.x * cs - fwdVec.y * sn, fwdVec.x * sn + fwdVec.y * cs);
-            
-			RaycastHit2D hitObj = Physics2D.Raycast(this.transform.position, rayDir,viewDist);
+            //MESH INIT
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            mesh.Clear();
+		    newVertices[0] = player.transform.position;
+		    // Debug.Log(newVertices.Length);
+		    newUV[0] = new Vector2(newVertices[0].x, newVertices[0].y);
 
-			if(hitObj.transform != null){
-				//red if hitting an object
-				player.transform.renderer.material.color = Color.red;
-				//Send a message to the obj hit to do it's thing
-				hitObj.transform.gameObject.SendMessage(player.transform.gameObject.tag+"Looking");
-				
-				//Add to new Mesh vert at point of contact
-				newVertices[intI+1] = new Vector3(hitObj.point.x,hitObj.point.y,0);
-				newUV[intI+1] = new Vector2(newVertices[intI+1].x, newVertices[intI+1].y);
-			}
-			else{
-			    //Otherwise add vert at farthest view distance
-			    newVertices[intI+1] = this.transform.position + new Vector3(
-			        viewDist*rayDir.x,
-					viewDist*rayDir.y,
-					0);
-				newUV[intI+1] = new Vector2(newVertices[intI+1].x, newVertices[intI+1].y);
-			}
-			//Add new triangle to mesh only if there are a least 2 verts
-    		if(i > 0){
-    			// Debug.Log(newTriangles.Length);
-    			// Debug.Log(intI);
-    			newTriangles[intI*3] = 0;
-                newTriangles[intI*3+1] = (intI+1)-1;//Please keep the (i+1) for clarity's sake
-                newTriangles[intI*3+2] = intI+1;
-    		}
-    		
-			//draw red lines
-			Debug.DrawLine(
-				this.transform.position,
-				this.transform.position + new Vector3(
-					viewDist*rayDir.x,
-					viewDist*rayDir.y,
-					0),
-				Color.red);
-				
-		this.transform.position = player.transform.position;
+        for(float i = 0f; i < playerFOV; i+=degBtwnRays) {//Raycast every 1 degree in FOV
+            //RAYCAST INIT
+            int intI = Mathf.FloorToInt(i);
+
+            float theta = Mathf.Deg2Rad*(-1*playerFOV/2+i);//Direction of vector
+            float cs = Mathf.Cos(theta);
+            float sn = Mathf.Sin(theta);
+
+            Vector3 fwdVec = player.transform.up;
+			Vector2 rayDir = new Vector2(fwdVec.x * cs - fwdVec.y * sn, fwdVec.x * sn + fwdVec.y * cs);
+
+            RaycastHit2D hitObj = Physics2D.Raycast(player.transform.position, rayDir,viewDist);
+            if(hitObj.transform != null){
+            	if(hitObj.transform.gameObject.tag == "Block"){
+                    //red if hitting an object
+                    player.transform.renderer.material.color = Color.red;
+                    //Send a message to the obj hit to do it's thing
+                    hitObj.transform.gameObject.SendMessage(player.transform.gameObject.tag+"Looking");
+                }   
+
+                    //Add to new Mesh vert at point of contact
+                    newVertices[intI+1] = new Vector3(hitObj.point.x,hitObj.point.y,0);
+                    newUV[intI+1] = new Vector2(newVertices[intI+1].x, newVertices[intI+1].y);
+            }
+            else{
+                //Otherwise add vert at farthest view distance
+                newVertices[intI+1] = player.transform.position + new Vector3(
+                    viewDist*rayDir.x,
+                            viewDist*rayDir.y,
+                            0);
+
+                    newUV[intI+1] = new Vector2(newVertices[intI+1].x, newVertices[intI+1].y);
+            }
+            //Add new triangle to mesh only if there are a least 2 verts
+	        if(i > 0){
+	            // Debug.Log(newTriangles.Length);
+	            // Debug.Log(intI);
+	            newTriangles[intI*3] = 0;
+				newTriangles[intI*3+1] = (intI+1)-1;//Please keep the (i+1) for clarity's sake
+				newTriangles[intI*3+2] = intI+1;
+	        }
+	        
+	        //draw red lines
+	        Debug.DrawLine(
+	        player.transform.position,
+	        player.transform.position + new Vector3(
+	                viewDist*rayDir.x,
+	                viewDist*rayDir.y,
+	                0),
+	        Color.red);
+	                
+	        //this.transform.position = new Vectorthis.transform.position;
 	    
-		}
-        //MODIFY MESH;
-        mesh.vertices = newVertices;
-        mesh.uv = newUV;
-        mesh.triangles = newTriangles;
-	}
-   
+        }
+    //MODIFY MESH;
+    mesh.vertices = newVertices;
+    mesh.uv = newUV;
+    mesh.triangles = newTriangles;
+    }
+
 }
--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Person : MonoBehaviour {
 	public float speed = 2.0f;
 	public float actionRange = 1.2f;
+	public bool useController = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -11,30 +12,38 @@ public class Person : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//wasd movement
-		//action button for this player
 		checkAction();
+		float angle, x, y, rtx, rty;
+		if(!useController){
+			//wasd movement
+			x = Input.GetAxis("HorizontalKeyboard") * Time.deltaTime * speed;
+			y = Input.GetAxis("VerticalKeyboard") * Time.deltaTime * speed * -1;
+			this.transform.Translate(x, y, 0, Space.World); //global axis
 
-		// Debug.Log(Input.GetAxis("L_XAxis_1") + " " + Time.deltaTime + " " + speed);
-		float x = Input.GetAxis("L_XAxis_1") * Time.deltaTime * speed;
-		float y = Input.GetAxis("L_YAxis_1") * Time.deltaTime * speed * -1;
-		this.transform.Translate(x, y, 0, Space.World); //global axis
+			rtx = Input.GetAxis("HorizontalKeyboard");
+			rty = -1*Input.GetAxis("VerticalKeyboard");
+			// Debug.Log(Input.GetAxis("L_XAxis_1") + " " + Time.deltaTime + " " + speed);
+			Vector3 mouse_pos = Input.mousePosition;
+	        Vector3 object_pos = Camera.main.WorldToScreenPoint(this.transform.position);
+			mouse_pos.x = mouse_pos.x - object_pos.x;
+			mouse_pos.y = mouse_pos.y - object_pos.y;
+			mouse_pos.z = Camera.main.transform.position.z - this.transform.position.z;
 
-		//look at mouse position
-		//The distance between the camera and object
-		// Vector3 mouse_pos = Input.mousePosition;
-        // Vector3 object_pos = Camera.main.WorldToScreenPoint(this.transform.position);
-		// mouse_pos.x = mouse_pos.x - object_pos.x;
-		// mouse_pos.y = mouse_pos.y - object_pos.y;
-		// mouse_pos.z = Camera.main.transform.position.z - this.transform.position.z;
-		// float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+			angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
 
-		//controller inputs
-		float rtx = Input.GetAxis("R_XAxis_1");
-		float rty = -1*Input.GetAxis("R_YAxis_1");
+		}else{
+			//controller inputs
+			x = Input.GetAxis("L_XAxis_1") * Time.deltaTime * speed;
+			y = Input.GetAxis("L_YAxis_1") * Time.deltaTime * speed * -1;
+			this.transform.Translate(x, y, 0, Space.World); //global axis
 
+			rtx = Input.GetAxis("R_XAxis_1");
+			rty = -1*Input.GetAxis("R_YAxis_1");
+
+			angle = Mathf.Atan2(rty, rtx) * Mathf.Rad2Deg;
+
+		}
 		//change the angle
-		float angle = Mathf.Atan2(rty, rtx) * Mathf.Rad2Deg;
 		this.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle - 90));
 	}
 
