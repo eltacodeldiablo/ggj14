53fbbefa539cd18bcfec55aad5ccb12ed306ee9b
eltacodeldiablo <smiley.we@gmail.com>
2014-01-25 11:13:07 -0500

    controller support

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -17,13 +17,20 @@ public class Person : MonoBehaviour {
 		this.transform.Translate(x, y, 0, Space.World); //global axis
 
 		//look at mouse position
-		Vector3 mouse_pos = Input.mousePosition;
 		//The distance between the camera and object
-		mouse_pos.z = Camera.main.transform.position.z - this.transform.position.z;
-		Vector3 object_pos = Camera.main.WorldToScreenPoint(this.transform.position);
-		mouse_pos.x = mouse_pos.x - object_pos.x;
-		mouse_pos.y = mouse_pos.y - object_pos.y;
-		float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+		// Vector3 mouse_pos = Input.mousePosition;
+        // Vector3 object_pos = Camera.main.WorldToScreenPoint(this.transform.position);
+		// mouse_pos.x = mouse_pos.x - object_pos.x;
+		// mouse_pos.y = mouse_pos.y - object_pos.y;
+		// mouse_pos.z = Camera.main.transform.position.z - this.transform.position.z;
+		// float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+
+		//controller inputs
+		float rtx = Input.GetAxis("RightH");
+		float rty = -1*Input.GetAxis("RightV");
+
+		//change the angle
+		float angle = Mathf.Atan2(rty, rtx) * Mathf.Rad2Deg;
 		this.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angle - 90));
 	}
 }
