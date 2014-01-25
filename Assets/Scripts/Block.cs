1ce10c9e4aacb3a384549e37d2a02ba85143c5db
neonwednesdays <dxiao7@gatech.edu>
2014-01-25 12:38:10 -0500

    block now reverts back to a default material when not being seen
    seenCounter
    render color fades between the two colors depending on colors

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class Block : MonoBehaviour {
-	public bool isMoveable;
+	public bool isMoveable;//is this a moveable block
+	bool isSeen = false;//is this current being seen
+
+	int seenMax = 30;//how long it should stay visible until its back to normal
+	int seenCounter = 0;//counter for being seen
 	// Use this for initialization
 	void Start () {
 	
@@ -14,11 +18,22 @@ public class Block : MonoBehaviour {
 		if(isMoveable){
 			this.transform.rotation = Quaternion.Euler(Vector3.zero);
 		}
+
+		this.transform.renderer.material.color = Color.Lerp(Color.white, Color.blue, (float)seenCounter/seenMax);
+		if(seenCounter == 0){//if its no longer being seen, set back to normal
+	   		//this.transform.renderer.material.color = Color.white;
+		}else{
+			seenCounter --;
+		}
 	}
 	
+	//seing seen by player 1
 	void Player1Looking(){
 	    this.transform.renderer.material.color = Color.blue;
+	    isSeen = true;
+	    seenCounter = seenMax;
 	}
+
 	void Player2Looking(){
 	    this.transform.renderer.material.color = Color.yellow;
 	}
